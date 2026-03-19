ac3502d2eb11c704aa753cc8a983636ed713201c
Priya2191-dev <lalepriyanka@gmail.com>
2026-03-19 16:46:35 +0530

    Update can_steps.py

--- features/steps/can_steps.py
+++ features/steps/can_steps.py
@@ -1,18 +1,28 @@
 from behave import given, when, then
 import can
+import time
 
 @given('a virtual CAN bus')
 def step_bus(context):
-    context.bus = can.Bus(interface='virtual', channel='test')
+    # Use SAME channel for TX and RX
+    context.bus_tx = can.Bus(interface='virtual', channel='test')
+    context.bus_rx = can.Bus(interface='virtual', channel='test')
 
 @when('I send a CAN message')
 def step_send(context):
     context.msg = can.Message(arbitration_id=0x123, data=[1,2,3,4])
-    context.bus.send(context.msg)
+    context.bus_tx.send(context.msg)
+
+    # IMPORTANT: allow time for message propagation
+    time.sleep(0.1)
 
 @then('I should receive the same CAN message')
 def step_receive(context):
-    received = context.bus.recv(timeout=1)
-    assert received is not None
+    received = context.bus_rx.recv(timeout=2)
+
+    assert received is not None, "No CAN message received"
     assert received.arbitration_id == context.msg.arbitration_id
-    context.bus.shutdown()
+
+    # Proper shutdown (fix warning)
+    context.bus_tx.shutdown()
+    context.bus_rx.shutdown()
