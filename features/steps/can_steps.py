163a45291de63cec2cc842636af80e6cd35c8f95
Priya2191-dev <lalepriyanka@gmail.com>
2026-04-03 09:18:00 +0530

    Update can_steps.py

--- features/steps/can_steps.py
+++ features/steps/can_steps.py
@@ -1,28 +1,54 @@
 from behave import given, when, then
 import can
 import time
+from can_simulator import get_bus, create_message, send_message, receive_message
+
 
 @given('a virtual CAN bus')
 def step_bus(context):
-    # Use SAME channel for TX and RX
-    context.bus_tx = can.Bus(interface='virtual', channel='test')
-    context.bus_rx = can.Bus(interface='virtual', channel='test')
+    context.bus = get_bus()
 
-@when('I send a CAN message')
-def step_send(context):
-    context.msg = can.Message(arbitration_id=0x123, data=[1,2,3,4])
-    context.bus_tx.send(context.msg)
+@when('I send a CAN message with ID {arb_id} and data {data}')
+def step_send(context, arb_id, data):
+    arb_id_int = int(arb_id, 16)
+    data_list = [int(x) for x in data.split(",")]
 
-    # IMPORTANT: allow time for message propagation
-    time.sleep(0.1)
+    context.msg = create_message(arbitration_id=arb_id_int, data=data_list)
+    context.send_status = send_message(context.bus, context.msg)
+
+    # Better than sleep → small polling wait
+    start = time.time()
+    while time.time() - start < 2:
+        context.received = receive_message(context.bus, timeout=0.2)
+        if context.received:
+            break
+
+@when('I do not send any CAN message')
+def step_no_send(context):
+    context.received = receive_message(context.bus, timeout=1)
 
 @then('I should receive the same CAN message')
-def step_receive(context):
-    received = context.bus_rx.recv(timeout=2)
+def step_validate_same(context):
+    assert context.send_status is True
+    assert context.received is not None, "No CAN message received"
 
-    assert received is not None, "No CAN message received"
-    assert received.arbitration_id == context.msg.arbitration_id
+    assert context.received.arbitration_id == context.msg.arbitration_id
+    assert list(context.received.data) == list(context.msg.data)
 
-    # Proper shutdown (fix warning)
-    context.bus_tx.shutdown()
-    context.bus_rx.shutdown()
+    context.bus.shutdown()
+
+@then('the received CAN message should match ID {arb_id} and data {data}')
+def step_validate_specific(context, arb_id, data):
+    arb_id_int = int(arb_id, 16)
+    data_list = [int(x) for x in data.split(",")]
+
+    assert context.received is not None
+    assert context.received.arbitration_id == arb_id_int
+    assert list(context.received.data) == data_list
+
+    context.bus.shutdown()
+
+@then('no CAN message should be received')
+def step_validate_none(context):
+    assert context.received is None
+    context.bus.shutdown()
