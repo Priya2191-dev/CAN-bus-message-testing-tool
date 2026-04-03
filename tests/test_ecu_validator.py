daf18b532c3edba8b975f194966dc713d181d2e1
Priya2191-dev <lalepriyanka@gmail.com>
2026-04-03 21:25:37 +0530

    Update test_ecu_validator.py

--- tests/test_ecu_validator.py
+++ tests/test_ecu_validator.py
@@ -1,9 +1,20 @@
 import pytest
 from ecu_validator import validate_speed
 
-def test_valid_speed():
-    assert validate_speed(60) == True
 
-def test_invalid_speed():
+def test_valid_speed():
+    assert validate_speed(60) is True
+
+@pytest.mark.parametrize("speed", [0, 50, 120])
+def test_boundary_values(speed):
+    assert validate_speed(speed) is True
+
+@pytest.mark.parametrize("speed", [-1, 121, 200])
+def test_invalid_speed_range(speed):
     with pytest.raises(ValueError):
-        validate_speed(200)
+        validate_speed(speed)
+
+@pytest.mark.parametrize("speed", ["fast", None, []])
+def test_invalid_speed_type(speed):
+    with pytest.raises(TypeError):
+        validate_speed(speed)
