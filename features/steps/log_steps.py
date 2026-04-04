46ded47cb388939fe9c55f3f7b2a16df254a72e1
Priya2191-dev <lalepriyanka@gmail.com>
2026-04-04 15:53:32 +0530

    Update log_steps.py

--- features/steps/log_steps.py
+++ features/steps/log_steps.py
@@ -1,14 +1,45 @@
 from behave import given, when, then
 from log_replay import replay
 
-@given('a CAN log')
-def step_log(context):
-    context.log = [{"id": 0x101, "data": [1,2]}]
+
+@given('a valid CAN log')
+def given_valid_log(context):
+    context.log = [{"id": 0x101, "data": [1, 2]}]
+    context.error = None
+
+@given('multiple CAN frames')
+def given_multiple(context):
+    context.log = [
+        {"id": 0x101, "data": [1, 2]},
+        {"id": 0x102, "data": [3, 4]},
+    ]
+    context.error = None
+
+
+@given('an invalid CAN log')
+def given_invalid(context):
+    context.log = "invalid"
+    context.error = None
+
 
 @when('I replay the log')
-def step_replay(context):
-    replay(context.log)
+def when_replay(context):
+    try:
+        replay(context.log)
+    except Exception as e:
+        context.error = e
 
-@then('messages should be printed')
-def step_output(context):
-    assert True 
+
+@then('CAN messages should be printed')
+def then_output(context):
+    assert context.error is None
+
+
+@then('all messages should be printed')
+def then_all_output(context):
+    assert context.error is None
+
+
+@then('replay should fail')
+def then_fail(context):
+    assert context.error is not None
