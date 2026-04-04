d7cd1be9aaf35b99a3a86a857f373250282b45b3
Priya2191-dev <lalepriyanka@gmail.com>
2026-04-04 12:15:01 +0530

    Update integrity_steps.py

--- features/steps/integrity_steps.py
+++ features/steps/integrity_steps.py
@@ -3,30 +3,30 @@ from integrity import checksum, verify
 
 
 @given('valid CAN data')
-def step_valid_data(context):
+def given_valid_data(context):
     context.data = [10, 20, 30]
     context.expected_checksum = checksum(context.data)
     context.result = None
 
 @given('CAN data [255, 1]')
-def step_overflow_data(context):
+def given_overflow_data(context):
     context.data = [255, 1]
     context.expected_checksum = checksum(context.data)
 
 @when('I calculate checksum')
-def step_calculate(context):
+def when_calculate(context):
     context.calculated = checksum(context.data)
     context.result = verify(context.data, context.expected_checksum)
 
 @when('I modify CAN data')
-def step_modify(context):
+def when_modify(context):
     context.data[0] = 99  # simulate corruption
     context.result = verify(context.data, context.expected_checksum)
 
 @then('integrity should be verified')
-def step_verified(context):
+def then_verified(context):
     assert context.result is True
 
 @then('integrity verification should fail')
-def step_failed(context):
+def then_failed(context):
     assert context.result is False
